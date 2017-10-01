9329527b43aa8739cd33ddbffb503064ce37eb76
Balvin Krishnan <kjbalvin@gmail.com>
2017-10-01 02:10:17 -0500

    Initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `<h1>
+  Hello {{name}}
+  <p>Email: {email}</p>
+  </h1>`,
 })
 export class AppComponent  { 
   name = 'John'; 
